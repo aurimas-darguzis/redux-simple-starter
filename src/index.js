81462a5bb6c73a5aedc5b5a340e99391071ec8f9
Aurimas <aurimas@darguzis.com>
2017-10-25 09:29:46 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAaUrcQV4PZObFU61mZXjsK39Dce2vC5Zw'
+
 // Create a new component. It should produce HTML
 const App = () => {
     return <div>Hi!</div>
